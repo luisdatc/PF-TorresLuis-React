9135b26b30d0adfe8bf5c508911fe45e87fd410f
Luis Daniel Torres <luisct91@gmail.com>
2023-04-18 00:23:06 -0300

    instalacion del react-router-dom y empezado de routeo en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import NavMenu from "./components/NavMenu/NavMenu";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import ItemListContainer from "./components/ItemlistContainer/ItemListContainer";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import MainSection from "./components/MainSection/MainSection";
 import ShipCardSales from "./components/ShipCardSales/ShipCardSales";
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavMenu />
       <MainSection />
       <ShipCardSales />
-      <ItemListContainer />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/detalle" element={<ItemDetailContainer />} />
+        {/* <Route path="/404NotFound" element={<404NotFound/>} /> CREAR EL COMPONENTE*/}
+        <Route path="*" element={<Navigate to="/" />} />
+        {/*         <Route path="*" element={<Navigate to="/404NotFound" />} />
+         */}
+      </Routes>
       <ShipCardSales />
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,18 @@
+import ItemDetail from '../ItemDetail/ItemDetail'
+
+const ItemDetailContainer = () => {
+//estado para guardar un producto
+//useeffect para llamar a la api o json para traer un producto y luego guardar en el estado 
+
+
+  return (
+    <div>
+        <ItemDetail
+        
+        //product={product}
+        />
+    </div>
+  )
+}
+
+export default ItemDetailContainer
